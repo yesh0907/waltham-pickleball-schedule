9da1a0f661320d8cc6662071f7d04b40754ab153
Yesh Chandiramani <yeshc0907@gmail.com>
2024-09-30 21:14:12 -0400

    process data in my own format

--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
 import { chromium } from "@playwright/test";
-import type { AvailabilityResponse } from "./types";
+import {
+  TimeSlots,
+  type AvailabilityResponse,
+  type CourtSchedule,
+  type Schedule,
+} from "./types";
 
 const BOOKING_PAGE =
   "https://anc.apm.activecommunities.com/walthamrecreation/reservation/landing/quick?locale=en-US&groupId=2";
 const AVAILABILITY_ENDPOINT =
   "https://anc.apm.activecommunities.com/walthamrecreation/rest/reservation/quickreservation/availability?locale=en-US";
 
-(async () => {
+function getTmrDate(): Date {
+  const tmr = new Date();
+  tmr.setDate(tmr.getDate() + 1);
+  return tmr;
+}
+
+// format date to be like the following 2024-10-19
+function formatDate(d: Date): string {
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+async function getAvailability(): Promise<AvailabilityResponse> {
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
@@ -14,18 +33,16 @@ const AVAILABILITY_ENDPOINT =
     waitUntil: "networkidle",
   });
 
-  // Get tomorrow's date in the format used by the availability API (e.g., "2024-10-19)
-  const tmr = new Date();
-  tmr.setDate(tmr.getDate() + 1);
-  const tmrFormattedDate = tmr.toISOString().split("T")[0];
+  // Get tomorrow's date in the format used by the availability API (e.g., 2024-10-19)
+  const tmr = getTmrDate();
+  const tmrFormattedDate = formatDate(tmr);
 
   // Get the CSRF token from the page
   const csrfToken = await page.evaluate(() => {
     return (window as any).__csrfToken || null;
   });
   if (!csrfToken) {
-    console.error("CSRF token not found");
-    return;
+    throw new Error("CSRF token not found");
   }
 
   // Prepare the request headers
@@ -46,39 +63,74 @@ const AVAILABILITY_ENDPOINT =
     change_time_range: false,
   };
 
-  // Make the fetch request
-  let res: AvailabilityResponse;
-  try {
-    res = await page.evaluate(
-      async ({
-        url,
-        headers,
-        body,
-      }: {
-        url: string;
-        headers: HeadersInit;
-        body: Record<string, any>;
-      }) => {
-        const resp = await fetch(url, {
-          method: "POST",
-          headers,
-          mode: "cors",
-          credentials: "include",
-          body: JSON.stringify(body),
-        });
-        return await resp.json();
-      },
-      {
-        url: AVAILABILITY_ENDPOINT,
-        headers,
-        body,
-      }
-    );
+  console.log(`Getting availability for ${tmrFormattedDate}`);
 
-    await Bun.write("schedule.json", JSON.stringify(res));
-  } catch (error) {
-    console.error("Error fetching availability:", error);
-  }
+  // Make the fetch request in the context of the webpage
+  const res: AvailabilityResponse = await page.evaluate(
+    async ({
+      url,
+      headers,
+      body,
+    }: {
+      url: string;
+      headers: HeadersInit;
+      body: Record<string, any>;
+    }) => {
+      const resp = await fetch(url, {
+        method: "POST",
+        headers,
+        mode: "cors",
+        credentials: "include",
+        body: JSON.stringify(body),
+      });
+      return await resp.json();
+    },
+    {
+      url: AVAILABILITY_ENDPOINT,
+      headers,
+      body,
+    }
+  );
 
   await browser.close();
-})();
+
+  return res;
+}
+
+async function main() {
+  const {
+    body: { availability },
+  } = await getAvailability();
+
+  // only care about Lowell courts with nets because thats where I play
+  const courts = availability.resources.filter((resource) =>
+    resource.resource_name.match(/^Lowell #\d+ Pickleball \(Nets\)$/)
+  );
+
+  const courtSchedules: CourtSchedule[] = courts.map((resource) => {
+    const schedule: Schedule = resource.time_slot_details.reduce(
+      (acc, timeSlot, idx) => {
+        const time = TimeSlots[idx];
+        return {
+          ...acc,
+          [time]: {
+            booked: !!timeSlot.status,
+          },
+        };
+      },
+      {} as Schedule
+    );
+
+    return {
+      name: resource.resource_name,
+      schedule,
+    } as CourtSchedule;
+  });
+
+  const tmrDate = formatDate(getTmrDate());
+
+  console.log(`court schedules for ${tmrDate}:`, courtSchedules);
+//   Bun.write("schedule.json", JSON.stringify(courtSchedules));
+}
+
+main();
--- types.ts
+++ types.ts
@@ -45,6 +45,15 @@ interface APIResponseHeaders {
 }
 
 export interface AvailabilityResponse {
-  headers: Headers;
-  body: Body;
+  headers: APIResponseHeaders;
+  body: AvailabilityBody;
+}
+
+export const TimeSlots = ["8AM", "10AM", "12PM", "2PM", "4PM", "6PM"] as const;
+export type TimeSlot = (typeof TimeSlots)[number];
+export type Schedule = Record<TimeSlot, { booked: boolean }>;
+
+export interface CourtSchedule {
+  name: string;
+  schedule: Schedule;
 }
